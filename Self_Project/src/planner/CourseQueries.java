c40bb99d2d39d325454a3485a42cc7d9cc14c23e
Ausgi <August.hermas@gmail.com>
2017-12-04 12:17:16 +0200

    Update CourseQueries.java

--- Self_Project/src/planner/CourseQueries.java
+++ Self_Project/src/planner/CourseQueries.java
@@ -11,9 +11,9 @@ import java.util.ArrayList;
 // This Class manages database operations associated to the Course Class
 public class CourseQueries {
     // DB connection details
-    private static final String URL = "jdbc:mysql://***"; // *** Azure database example "jdbc:mysql://HOSTNAME:PORT/DATABASE_NAME"
-    private static final String USERNAME = "***"; // Database username
-    private static final String PASSWORD = "***"; // Database password
+    private static final String URL = "jdbc:mysql://***"; // ***Azure database example "jdbc:mysql://HOSTNAME:PORT/DATABASE_NAME"
+    private static final String USERNAME = "**"; // **Database username
+    private static final String PASSWORD = "*"; // *Database password
  
     private Connection connection = null;
     private static PreparedStatement selectAllCourses = null;
